023002b8bcc6cf684dd95a6d3ea63605c8bb2ded
Horacio Torres Galindo <thoraciog@gmail.com>
2021-10-10 16:08:05 -0600

    made hero themes

--- week5/Superhero/config/urls.py
+++ week5/Superhero/config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hero.views import HeroView, HeroDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HeroView.as_view()),
+    path('<int:pk>', HeroDetailView.as_view()),
 ]
--- week5/Superhero/hero/views.py
+++ week5/Superhero/hero/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Hero
+
+class HeroView(ListView):
+    template_name = 'hero_list.html'
+    model = Hero
+
+class HeroDetailView(DetailView):
+    template_name = 'hero_detail.html'
+    model = Hero
+
 
 # Create your views here.
