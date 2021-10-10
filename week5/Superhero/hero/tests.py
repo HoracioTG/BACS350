de1811e91fb9cda36d3f0b2d0b10961b608386fc
Horacio Torres Galindo <thoraciog@gmail.com>
2021-10-10 16:41:45 -0600

    added tests

--- week5/Superhero/hero/tests.py
+++ week5/Superhero/hero/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Hero
 
-# Create your tests here.
+class HeroTest(TestCase):
+    def test_django (self):
+        self.assertTrue 
+    
+    def test_num_hero(self):
+        self.assertEqual(len(Hero.objects.all()), 0)
+    
+    def test_add_hero(self):
+        Hero.objects.create(name='The Flash', description = 'Im The Flash, zOOOO000oooooom')
+        self.assertEqual(len(Hero.objects.all()), 1)
